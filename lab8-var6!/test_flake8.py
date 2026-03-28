bb1f7c5e2061cd4b40e31d59489071846db4f69d
angelina00001 <79416823+angelina00001@users.noreply.github.com>
2026-03-28 10:38:15 +0300

    Create test_flake8.py

---
+++ lab8-var6!/test_flake8.py
@@ -0,0 +1,16 @@
+def test_add():
+    assert add(2,3)==5
+
+def test_long_name_function_with_many_parameters(param1,param2,param3,param4,param5):
+    pass
+
+import pytest
+import os, sys
+import requests
+
+class TestClass:
+    def test_one(self):assert True
+    
+    def test_two(self):
+        x=1
+        if x==1:print("test")
